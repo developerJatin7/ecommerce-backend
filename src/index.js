35e9cbd81d22f2108d6097385de11055f4d48842
developerJatin7 <jatinsuryawanshi725@gmail.com>
2026-08-26 21:51:11 +0530

    feat: set up MongoDB connection and update server initialization
    - Added a new `constants.js` file to store the database name.
    - Created a `db/index.js` file to handle MongoDB connection using Mongoose.
    - Updated `index.js` to include dotenv configuration and connect to MongoDB before starting the server.
    - Modified `package.json` to include necessary dependencies and a development script using nodemon.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-import app from "./app.js";
-
-const PORT = 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+import app from './app.js';
+dotenv.config({path:'.env'});
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 5000, () => {
+        console.log(`Server is running on: ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection failed !!!",err);
+})
